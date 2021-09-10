8880a1a0e2a120f59bfeb01e8bfec2b5ca796ca5
Gleb Golotvin <golotvin@ians.aero>
2021-09-10 13:19:01 +0300

    готовая версия, надеюсь

--- test_items.py
+++ test_items.py
@@ -1,13 +1,13 @@
 import pytest
+import time
 from selenium import webdriver
 
 class TestMainPage1():
-    def add_to_basket_btn_seen(self, browser, language):
+    def test_add_to_basket_btn_seen(self, browser):
         link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
           
         browser.get(link)
        
-
         correct_text_elt = browser.find_element_by_css_selector("button.btn-add-to-basket")
         correct_text = correct_text_elt.text
         assert correct_text, \
